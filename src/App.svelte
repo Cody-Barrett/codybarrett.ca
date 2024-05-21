2ca71a7c6acd6d8f49e5acb20dd344a6b912d7fd
Cody Barrett <github@codybarrett.ca>
2024-05-21 00:16:09 -0300

    better support for small screens and some style changes

--- src/App.svelte
+++ src/App.svelte
@@ -23,14 +23,14 @@
     </div>
   </header>
   <body>
-    <div class="bg-gradient-to-tr from-[#CEFCBA] via-sky-500 to-fuchsia-600 text-white h-full p-16">
+    <div class="bg-gradient-to-tr from-[#CEFCBA] via-sky-500 to-fuchsia-600 text-white h-full py-16">
       <div class="text-center">
         <h2 class="text-3xl font-bold drop-shadow-lg">
           Hi, I'm <span class="underline decoration-white underline-offset-4">Cody</span>.
         </h2>
-        <p class="text-lg drop-shadow-md pt-2">I'm an IT Specialist with expertise in Software Development, Network/System Administration, and Cybersecurity.</p>
+        <p class="px-10 text-lg drop-shadow-md pt-2">I'm an IT Specialist with expertise in Software Development, Network/System Administration, and Cybersecurity.</p>
       </div>
-      <div class="pt-12 max-w-max mx-auto grid xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-3 grid-cols-2 text-black gap-4">
+      <div class="pt-12 px-2 max-w-max mx-auto grid xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-4 min-[320px]:grid-cols-3 grid-cols-2 text-black gap-3">
         <SVGCard name="Python"></SVGCard>
         <SVGCard name="C++"></SVGCard>
         <SVGCard name="NodeJS"></SVGCard>
@@ -39,11 +39,14 @@
         <SVGCard name="PostgreSQL"></SVGCard>
         <SVGCard name="MongoDB"></SVGCard>
         <SVGCard name="Redis"></SVGCard>
-        <SVGCard name="Git"></SVGCard>
-        <SVGCard name="TailwindCSS"></SVGCard>
-        <SVGCard name="Vite"></SVGCard>
-        <SVGCard name="Svelte"></SVGCard>
         <SVGCard name="Linux"></SVGCard>
+        <SVGCard name="AWS"></SVGCard>
+        <SVGCard name="Docker"></SVGCard>
+        <SVGCard name="Git"></SVGCard>
+        <SVGCard name="Github Actions"></SVGCard>
+        <SVGCard name="TailwindCSS"></SVGCard>
+        <SVGCard name="Svelte"></SVGCard>
+        <SVGCard name="Vite"></SVGCard>
         <SVGCard name="Office"></SVGCard>
       </div>
     </div>
--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -6,11 +6,11 @@
   export let link_text;
 </script>
 
-<div class="col-span-1 mx-auto max-w-lg rounded-xl w-full drop-shadow-lg hover:drop-shadow-2xl transition duration-500 bg-white border p-3">
+<div class="col-span-1 mx-auto max-w-lg rounded-xl w-full drop-shadow-lg hover:drop-shadow-2xl transition duration-500 bg-white border hover:border-black p-3">
   <img class="mx-auto w-full h-40 rounded-xl object-center object-cover border border-slate-300" src={thumbnail} alt="Project Thumbnail" />
   <div class="px-2 py-4">
-    <div class="font-bold md:text-xl text-md mb-2">{header}</div>
-    <p class="text-gray-700 text-md">{description}</p>
+    <div class="font-bold md:text-xl sm:text-md text-sm mb-2">{header}</div>
+    <p class="text-gray-700 sm:text-md text-sm">{description}</p>
   </div>
   <a href={link} class="block">
     <div class="p-4 drop-shadow-sm bg-gray-100 text-center rounded-xl text-blue-500 hover:text-blue-900 hover:bg-slate-200 transition duration-500 hover:shadow-inner border-black">
