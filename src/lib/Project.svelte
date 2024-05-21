4529808aad81ebd7577cadc20fbec4683b286378
Cody Barrett <github@codybarrett.ca>
2024-05-21 05:08:36 -0300

    removed hover effects on project cards from smaller devices, changed text wrapping

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -6,14 +6,14 @@
   export let link_text;
 </script>
 
-<div class="col-span-1 mx-auto max-w-lg rounded-xl w-full drop-shadow-lg hover:drop-shadow-2xl transition duration-500 bg-white border hover:border-black p-3">
-  <img class="mx-auto w-full h-40 rounded-xl object-center object-cover border border-slate-300" src={thumbnail} alt="Project Thumbnail" />
-  <div class="px-2 py-4">
-    <div class="font-bold md:text-xl sm:text-md text-sm mb-2">{header}</div>
-    <p class="text-gray-700 sm:text-md text-sm">{description}</p>
+<div class="w-full h-full max-w-lg col-span-1 p-3 mx-auto transition duration-500 bg-white border rounded-xl drop-shadow-lg sm:hover:drop-shadow-2xl sm:hover:border-black">
+  <img class="object-cover object-center w-full h-40 mx-auto border rounded-xl border-slate-300" src={thumbnail} alt="Project Thumbnail" />
+  <div class="px-2 pt-4 h-36">
+    <div class="mb-2 text-sm font-bold md:text-xl sm:text-md">{header}</div>
+    <p class="overflow-hidden text-sm text-gray-700 text-ellipsis line-clamp-4 sm:text-md">{description}</p>
   </div>
-  <a href={link} class="block">
-    <div class="p-4 drop-shadow-sm bg-gray-100 text-center rounded-xl text-blue-500 hover:text-blue-900 hover:bg-slate-200 transition duration-500 hover:shadow-inner border-black">
+  <a href={link} class="block m-auto">
+    <div class="p-4 text-center text-blue-500 transition duration-500 bg-gray-100 border-black drop-shadow-sm rounded-xl sm:hover:text-blue-900 sm:hover:bg-slate-200 md:hover:shadow-inner">
       <span class="inline-flex items-center">{link_text}</span>
     </div>
   </a>
